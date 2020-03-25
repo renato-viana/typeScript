ef437af4cff51d1f536d067609f84d90adc2d858
Renato Viana <renatoviana30@gmail.com>
2020-03-25 15:25:49 -0300

    DOM Injector e Lazy loading

--- app/js/helpers/decorators/index.js
+++ app/js/helpers/decorators/index.js
@@ -1,4 +1,4 @@
-System.register(["./logarTempoDeExecu\u00E7\u00E3o"], function (exports_1, context_1) {
+System.register(["./logarTempoDeExecu\u00E7\u00E3o", "./domInject"], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     function exportStar_1(m) {
@@ -12,6 +12,9 @@ System.register(["./logarTempoDeExecu\u00E7\u00E3o"], function (exports_1, conte
         setters: [
             function (logarTempoDeExecu__o_1_1) {
                 exportStar_1(logarTempoDeExecu__o_1_1);
+            },
+            function (domInject_1_1) {
+                exportStar_1(domInject_1_1);
             }
         ],
         execute: function () {
